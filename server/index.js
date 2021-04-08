0aaf55be08a0bc2e283ce35cb356caf2a7b6e752
samuVillegas <villegassamuel25@gmail.com>
2021-04-08 08:58:20 -0500

    Se crea la documentacion con swagger

--- server/index.js
+++ server/index.js
@@ -3,7 +3,31 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes/routes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 require('dotenv').config();
+
+
+
+//Config swagger
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:8085",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 
 app.use(morgan('dev'));
@@ -11,6 +35,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/api', routes);
+
+
 
 
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,150 @@
-const{Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 const {
     getMain,
     getProducts,
     createSale,
     getSales
-}= require('../controller/controller');
+} = require('../controller/controller');
 
-router.get('/',getMain);
-router.get('/getProducts',getProducts);
-router.post('/createSale',createSale);
-router.get('/getSales',getSales);
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Sale:
+ *          type: object
+ *          required:
+ *              -NumbreSale
+ *              -SubTotal
+ *              -TotalIVA
+ *              -CreationDate
+ *              -NameUser
+ *              -Total
+ *          properties:
+ *              NumbreSale:
+ *                  type: integer
+ *                  description: Identifier of a sale
+ *              SubTotal:
+ *                  type: integer
+ *                  description: Subtotal of sale
+ *              TotalIVA:
+ *                  type: integer
+ *                  description: TotalIVA of sale
+ *              CreationDate: 
+ *                  type: date
+ *                  description: Date and time of creation of the sale
+ *              NameUser:
+ *                  type: string
+ *                  description: Name of the user who made the purchase
+ *              Total:
+ *                  type: integer
+ *                  description: Total of sale
+ *           
+ *          example:
+ *              NumbreSale: 1
+ *              SubTotal: 90000
+ *              TotalIVA: 17100
+ *              CreationDate: 20210501170500
+ *              NameUser: Fernando
+ *              Total: 107100
+ *      Product:
+ *          type: object
+ *          required:
+ *              -Id
+ *              -Description
+ *              -Price
+ *              -Existence
+ *          properties:
+ *              Id: 
+ *                  type: integer
+ *                  description: Product identifier
+ *              Description:
+ *                  type: string
+ *                  description: Short description of the product
+ *              Price:
+ *                  type: integer
+ *                  description: Price of product
+ *              Existence: 
+ *                  type: integer
+ *                  description: Number of existing products
+ * 
+ *          example:
+ *              Id: 1
+ *              Description: Foam
+ *              Price: 17000
+ *              Existence: 1000
+ *                     
+ *      
+ *                
+ */
+
+router.get('/', getMain);
+
+
+
+/**
+ * @swagger
+ * /getProducts:
+ *  get:
+ *      summary: Get the products stored in the JSON
+ *      tags: [Product]
+ *      responses:
+ *          275:
+ *              description: A JSON with the products and their respective values
+ *              content:
+ *                  application/json:
+ *                          schema: 
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Product'                   
+ *          255:
+ *              description: Server error
+ * 
+ */
+router.get('/getProducts', getProducts);
+
+/**
+ * @swagger
+ * /createSale:
+ *  post:
+ *      summary: Create one sale in the DB
+ *      tags: [Sale]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema: 
+ *                      $ref: '#/components/schemas/Sale'  
+ *      responses:
+ *          265:
+ *              description: Sale successfully created
+ *          255:
+ *              description: Server error
+ * 
+ */
+router.post('/createSale', createSale);
+
+/**
+ * @swagger
+ * /getSales:
+ *  get:
+ *      summary: Get the sales stored in the DB
+ *      tags: [Sale]
+ *      responses:
+ *          275:
+ *              description: A JSON is returned with the sales and their respective values
+ *              content:
+ *                  application/json:
+ *                          schema: 
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Sale'  
+ *          285: 
+ *              description: There are no sales
+ *          255:
+ *              description: Server error
+ * 
+ */
+router.get('/getSales', getSales);
 
 module.exports = router;
